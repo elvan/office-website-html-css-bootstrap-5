dcf0c18cdbd9a2cc666ecc94a6b8417fd647507d
Elvan Hidayat <elvanhd@gmail.com>
2024-01-01 11:09:28 +0700

    back-to-top button

--- js/script.js
+++ js/script.js
@@ -22,5 +22,27 @@ function incrementStats() {
   });
 }
 
+function userScroll() {
+  const navbar = document.querySelector(".navbar");
+  const toTopBtn = document.querySelector("#to-top");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+      navbar.classList.add("navbar-sticky");
+      toTopBtn.classList.add("show");
+    } else {
+      navbar.classList.remove("navbar-sticky");
+      toTopBtn.classList.remove("show");
+    }
+  });
+}
+
+function scrollToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
 // Event Listeners
 document.addEventListener("DOMContentLoaded", incrementStats);
+document.addEventListener("DOMContentLoaded", userScroll);
+document.querySelector("#to-top").addEventListener("click", scrollToTop);
